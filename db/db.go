a405e540420b9e6742e5b87b12b836d0bf55751e
Antoine Aube <aaube3@gmail.com>
2024-02-27 10:52:37 -0700

    New connection function with the db package

--- db/db.go
+++ db/db.go
@@ -1,6 +1,22 @@
 package database
 
 import (
-	// "database/sql"
+	"database/sql"
 	_"github.com/lib/pq"
 )
+
+func NewConnection() (*sql.DB, error) {
+	connStr := "user=antoine dbname=jurnolgolang password=papichulo33 host=localhost port=5432 sslmode=disable"
+
+		db, err := sql.Open("postgres", connStr)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if err = db.Ping(); err != nil {
+			return nil, err
+		}
+
+		return db, nil
+}
