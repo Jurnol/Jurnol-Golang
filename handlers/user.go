0ee6bcdcdc2bcbfc2f5cd3b2407148ee38183230
Antoine Aube <aaube3@gmail.com>
2024-02-27 12:03:19 -0700

    create user handler function for adding user to db

--- handlers/user.go
+++ handlers/user.go
@@ -3,10 +3,10 @@ package handlers
 import (
 	"database/sql"
     "encoding/json"
-    "fmt"
+    // "fmt"
     "log"
     "net/http"
-    "jurnol/db"
+    // "jurnol/db"
     "jurnol/models"
 )
 
@@ -19,5 +19,16 @@ func CreateUserHandler(db *sql.DB) http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+
+		query := `INSERT INTO users (first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4)`
+		_, err = db.Exec(query, newUser.FirstName, newUser.LastName, newUser.Email, newUser.PasswordHash) 
+		if err != nil {
+			log.Printf("Failed to insert new user: %v", err)
+			http.Error(w, "failed to create new user", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]string{"message" : " User created successfully"})
 	}
 }
